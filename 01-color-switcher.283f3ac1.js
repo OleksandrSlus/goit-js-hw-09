2c4959c9afde4b10e83404204e65f6ccc5035d22
OleksandrSlus <121134310+OleksandrSlus@users.noreply.github.com>
2023-05-25 12:49:54 +0000

    Deploying to gh-pages from @ OleksandrSlus/goit-js-hw-09@d80036509dd5ec954b11425082f864bfb9f51b6f 🚀

---
+++ 01-color-switcher.283f3ac1.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),bodyEl:document.body},n=null;t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,n=setInterval((function(){t.bodyEl.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)}));t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.283f3ac1.js.map
